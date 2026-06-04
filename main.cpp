d80b37f35fc4e228525831477e51ae75dee9d405
danu <danuecumaster@gmail.com>
2026-06-04 15:02:06 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,70 +25,76 @@
 #include <SPI.h>
 #include "JetBrainsMonoBold50pt7b.h"
 
-#define TFT_MOSI  23
-#define TFT_SCLK  18
-#define TFT_CS    15
-#define TFT_DC    2
-#define TFT_RST   4
-#define BL        32
-#define BG_COLOR  0x0841
-#define TXT_COLOR ST77XX_WHITE
-#define WB_PIN 	  33
+#define TFT_MOSI  		23
+#define TFT_SCLK  		18
+#define TFT_CS    		15
+#define TFT_DC    		2
+#define TFT_RST   		4
+#define BL        		32
+#define SCREEN_W	 	320
+#define SCREEN_H	 	170
+#define FRAME_TIME_MS 	33
+
+#define WB_PIN 	  		33
+#define V_DIVIDE  		2.0f
+#define AFR_DIVIDE  	2.0f
+#define ADC_MAX_COUNTS 	4095.0f
+#define ADC_REF_VOLTAGE 3.3f
+
+#define BG_COLOR  		0x0841
+#define TXT_COLOR 		ST77XX_WHITE
+#define AFR_MIN		 	10.0f
+#define AFR_MAX		 	20.0f
+#define AFR_RICH	 	12.0f
+#define AFR_STOICH	 	15.0f
+#define ADC_SAMPLES		16
+
+#define NUM_BOXES 		11
+#define GAP 			2
+#define BOX_HEIGHT 		28
+#define INACTIVE_COLOR  0x0861
 
 Adafruit_ST7789 tft(TFT_CS, TFT_DC, TFT_RST);
-GFXcanvas16 canvas(320, 170);
+GFXcanvas16 canvas(SCREEN_W, SCREEN_H);
 
 int textX;
 int textY;
 char buf[10];
 
-void drawTopBoxes(GFXcanvas16 &c, float afr) {
-	const int numBoxes 		= 11;
-    const int gap 			= 2;
-    const int boxHeight 	= 28;
-    uint16_t inactiveColor  = 0x0861;
+void drawTopBoxes(GFXcanvas16 &c, float afr) {	
     int activeBoxes;
+	uint16_t activeColor;
 
-	if (afr < 10.0f) {
+	if (afr < AFR_MIN) {
         activeBoxes = 0;
 	} else {
-        activeBoxes = min(11, (int)(afr - 9.0));
+        activeBoxes = min(NUM_BOXES, (int)(afr - (AFR_MIN - 1.0f)));
+	}
+	
+	if (afr < AFR_RICH) {
+		activeColor = ST77XX_BLUE;
+	} else if (afr < AFR_STOICH) {
+		activeColor = ST77XX_GREEN;
+	} else {
+		activeColor = ST77XX_RED;
 	}
 
-    for (int i = 0; i < numBoxes; i++) {
-        uint16_t activeColor;
-        
-        if (afr < 12.0f) {
-            activeColor = ST77XX_BLUE;
-		} else if (afr < 15.0f) {
-            activeColor = ST77XX_GREEN;
-		} else {
-            activeColor = ST77XX_RED;
-		}
+    for (int i = 0; i < NUM_BOXES; i++) {                
+        int x1 = (SCREEN_W * i) / NUM_BOXES;
+        int x2 = (SCREEN_W * (i + 1)) / NUM_BOXES;
 
-        int x1 = (320 * i) / numBoxes;
-        int x2 = (320 * (i + 1)) / numBoxes;
-
-        x1 += gap / 2;
-        x2 -= gap / 2;
-
+        x1 += GAP / 2;
+        x2 -= GAP / 2;
         int w = x2 - x1;
 
-        c.fillRoundRect(
-            x1,
-            4,
-            w,
-            boxHeight,
-            4,
-            (i < activeBoxes) ? activeColor : inactiveColor
-        );
+        c.fillRoundRect( x1, 4, w, BOX_HEIGHT, 4, (i < activeBoxes) ? activeColor : INACTIVE_COLOR );
     }
 }
 
 void setup() {
     Serial.begin(115200);
 
-    tft.init(170, 320);
+    tft.init(SCREEN_H, SCREEN_W);
     tft.setRotation(3);
     tft.fillScreen(BG_COLOR);
 	
@@ -107,25 +113,26 @@ void setup() {
 
     int16_t x1, y1;
     uint16_t w, h;
-    canvas.getTextBounds("20.00", 0, 0, &x1, &y1, &w, &h);
+    canvas.getTextBounds("00.00", 0, 0, &x1, &y1, &w, &h);
 
-    textX = (320 - w) / 2 - x1;
-    textY = ((170 - 36) - h) / 2 - y1 + 36;
+    textX = (SCREEN_W - w) / 2 - x1;
+    textY = ((SCREEN_H - 36) - h) / 2 - y1 + 36;
 }
 
-void loop() {   
-	uint32_t sum 	= 0;
-	float afr_value = 0.0f;
+void loop() {  
+	uint32_t start_loop	= millis();
+	uint32_t sum 		= 0;
+	float afr_value		= 0.0f;	
 	
-	for (int i = 0; i < 16; i++) {
+	for (int i = 0; i < ADC_SAMPLES; i++) {
         sum += analogRead(WB_PIN);
     }
-    int raw 		= (sum / 16);
-	float voltage	= ((raw / 4095.0f) * 3.3f);
-	float wbVoltage = (voltage * 2.0f);
-	afr_value 		= (10.0f + (wbVoltage * 2.0f));
+    uint16_t raw 	= (sum / ADC_SAMPLES);
+	float voltage	= (raw / ADC_MAX_COUNTS * ADC_REF_VOLTAGE);
+	float wbVoltage = (voltage * V_DIVIDE);
+	afr_value 		= (AFR_MIN + (wbVoltage * AFR_DIVIDE));
 	
-	if(afr_value < 10.00 || afr_value > 20.00) {
+	if(afr_value < AFR_MIN || afr_value > AFR_MAX) {
 		strcpy(buf, "ERR");
 		afr_value = 0.0f;
 	} else {
@@ -135,7 +142,10 @@ void loop() {
 	drawTopBoxes(canvas, afr_value);
 	canvas.setCursor(textX, textY);
 	canvas.print(buf);
-	tft.drawRGBBitmap(0, 0, canvas.getBuffer(), 320, 170);	
-	
-    delay(33);	//30FPS. ADJUST AS NEEDED. 
+	tft.drawRGBBitmap(0, 0, canvas.getBuffer(), SCREEN_W, SCREEN_H);	
+
+	uint32_t end_loop = (millis() - start_loop);
+    if (end_loop < FRAME_TIME_MS) {
+		delay(FRAME_TIME_MS - end_loop); //30FPS. ADJUST AS NEEDED. 
+	}	
 }
