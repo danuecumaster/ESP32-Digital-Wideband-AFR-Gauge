f82c43336770101d0b6a0a51767bbd70bfa5a8d7
danu <danuecumaster@gmail.com>
2026-06-15 09:19:36 +0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,18 +1,13 @@
 // -----------------------------------------------------------------------------
-// WIDEBAND ANALOG INPUT (GPIO33)
-//
-// ESP32 ADC INPUTS ARE NOT 5V TOLERANT.
-// DO NOT CONNECT DIRECTLY TO GPIO33.
-//
 //   WB 0-5V
 //      |
 //     10K
 //      |
-//      +---- 1K ----+----> GPIO33
-//      |            |
-//     10K         100NF
-//      |            |
-//     GND          GND
+//      +---- 1K ----+----> ADS1115 A0 ----> ESP32 I2C
+//      |            |		  |				 
+//     10K         100NF      | 			 
+//      |            |		  |				 
+//     GND          GND		 GND			
 //
 // DIVIDER: 10K / 10K  -> 0-5V BECOMES APPROXIMATELY 0-2.5V
 // FILTER : 1K + 100NF -> REDUCES ADC NOISE
@@ -23,42 +18,59 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
 #include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
 #include "JetBrainsMonoBold50pt7b.h"
 
+/**** DISPLAY PINS ****/
 #define TFT_MOSI  		23
 #define TFT_SCLK  		18
 #define TFT_CS    		15
 #define TFT_DC    		2
 #define TFT_RST   		4
 #define BL        		32
+/**** DISPLAY PINS ****/
+
+/**** I2C PINS ****/
+#define SDA_PIN   		21
+#define SCL_PIN   		22
+/**** I2C PINS ****/
+
+/**** SCREEN SETTINGS ****/
 #define SCREEN_W	 	320
 #define SCREEN_H	 	170
-#define FRAME_TIME_MS 	50 				//100 = 10FPS, 50 = 20FPS, 33 = 30FPS
+#define FRAME_TIME_MS 	50 				// 100 = 10FPS, 50 = 20FPS, 33 = 30FPS
+/**** SCREEN SETTINGS ****/
 
-#define WB_PIN 	  		33				//WIDEBAND PIN
-#define ADC_SAMPLES		16				//NUMBER OF ADC SAMPLES
-#define V_DIVIDE  		2.0f
-#define AFR_DIVIDE  	2.0f
-#define ADC_MAX_COUNTS 	4095.0f
-#define ADC_REF_VOLTAGE 3.3f
-#define ADC_GAIN		1.0000f			//GPIO CALIBRATION AND ADJUSTMENT
+/**** ADC SETTINGS ****/
+#define ADC_SAMPLES		2				// NUMBER OF ADC SAMPLES
+#define ADC_CORRECTION	1.0000f			// ADC CALIBRATION FACTOR
+#define V_MULTIPLIER	2.0f			// COMPENSATE FOR 10K/10K DIVIDER
+#define WBO_MULTIPLIER	2.0f			// WBO CONTROLLER MULTIPLIER 
+#define AFR_TOLERANCE	0.5f			// ERROR THRESHOLD
+/**** ADC SETTINGS ****/
 
+/**** AFR BOX SETTINGS ****/
 #define BG_COLOR  		0x0841
 #define TXT_COLOR 		ST77XX_WHITE
 #define AFR_MIN		 	10.0f
 #define AFR_MAX		 	20.0f
 #define AFR_RICH	 	12.0f
 #define AFR_STOICH	 	15.0f
-
 #define NUM_BOXES 		11
 #define GAP 			2
 #define BOX_HEIGHT 		28
 #define INACTIVE_COLOR  0x0861
+/**** AFR BOX SETTINGS ****/
 
+/**** DEBUG ****/
 #define DEBUG_MODE  	1 				//1 = ON, 0 = OFF
+/**** DEBUG ****/
 
 Adafruit_ST7789 tft(TFT_CS, TFT_DC, TFT_RST);
 GFXcanvas16 canvas(SCREEN_W, SCREEN_H);
+
+Adafruit_ADS1115 ads;
 
 int textX;
 int textY;
@@ -100,13 +112,6 @@ void setup() {
     tft.init(SCREEN_H, SCREEN_W);
     tft.setRotation(3);
     tft.fillScreen(BG_COLOR);
-	
-	//analogReadResolution(12);
-	pinMode(WB_PIN, INPUT);
-	analogSetWidth(12);	
-    analogSetPinAttenuation(WB_PIN, ADC_11db);
-	analogRead(WB_PIN);
-	delay(10);
 
     pinMode(BL, OUTPUT);
     digitalWrite(BL, HIGH);
@@ -120,40 +125,49 @@ void setup() {
 
     textX = (SCREEN_W - w) / 2 - x1;
     textY = ((SCREEN_H - 36) - h) / 2 - y1 + 36;
+	
+	Wire.begin(SDA_PIN, SCL_PIN);
+	Wire.setClock(400000);
+	if (!ads.begin()) {
+        Serial.println("ADS1115 not found");
+        while (1);
+    }
+	ads.setGain(GAIN_ONE);
+	Serial.println("ADS1115 initialized");
 }
 
 void loop() {  
-	uint32_t start_loop	= millis();
-	uint32_t sum 		= 0;
-	float afr_value		= 0.0f;	
+	uint32_t startLoop	= millis();	
+	float sum 			= 0.0f;
+	float afr			= 0.0f;
 	
 	for (int i = 0; i < ADC_SAMPLES; i++) {
-        sum += analogRead(WB_PIN);
+		int16_t adc = ads.readADC_SingleEnded(0);
+        sum += ads.computeVolts(adc);
     }
-    float raw 		= ((float)sum / ADC_SAMPLES);
-	float voltage	= (raw / ADC_MAX_COUNTS * ADC_REF_VOLTAGE * ADC_GAIN);
-	float wbVoltage = (voltage * V_DIVIDE);
-	afr_value 		= (AFR_MIN + (wbVoltage * AFR_DIVIDE));
 	
-	// DEBUG MODE - PRINT IF 1
+    float rawV			= ((float)sum / ADC_SAMPLES);
+	float voltage		= (rawV * V_MULTIPLIER * ADC_CORRECTION);
+	afr					= (AFR_MIN + (voltage * WBO_MULTIPLIER));
+	
 	#if DEBUG_MODE
-		Serial.printf("raw=%.2f gpio=%.3fV wb=%.3fV AFR=%.2f\n", raw, voltage, wbVoltage, afr_value);
+		Serial.printf("ADC=%.6f WB=%.4fV AFR=%.2f\n", rawV, voltage, afr);
 	#endif
 	
-	if(afr_value < AFR_MIN || afr_value > AFR_MAX) {
+	if(afr < (AFR_MIN - AFR_TOLERANCE) || afr > (AFR_MAX + AFR_TOLERANCE)) {
 		strcpy(buf, "ERROR");
-		afr_value = 0.0f;
+		afr = 0.0f;
 	} else {
-		snprintf(buf, sizeof(buf), "%.2f", afr_value);			
+		snprintf(buf, sizeof(buf), "%.2f", afr);			
 	}
 	canvas.fillScreen(BG_COLOR);
-	drawTopBoxes(canvas, afr_value);
+	drawTopBoxes(canvas, afr);
 	canvas.setCursor(textX, textY);
 	canvas.print(buf);
 	tft.drawRGBBitmap(0, 0, canvas.getBuffer(), SCREEN_W, SCREEN_H);	
 
-	uint32_t end_loop = (millis() - start_loop);
-    if (end_loop < FRAME_TIME_MS) {
-		delay(FRAME_TIME_MS - end_loop); //20FPS. ADJUST AS NEEDED. 
+	uint32_t endLoop = (millis() - startLoop);
+    if (endLoop < FRAME_TIME_MS) {
+		delay(FRAME_TIME_MS - endLoop);
 	}	
 }
